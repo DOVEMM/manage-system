8ff5fe98e72bca446bc35457a089614dab4d32dc
liuqingbo <1024195678@qq.com>
2018-05-16 17:53:53 +0800

    修改了路由页面

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -20,85 +20,85 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                items: [
-                    {
-                        icon: 'el-icon-setting',
-                        index: 'readme',
-                        title: '自述'
-                    },
-                    {
-                        icon: 'el-icon-menu',
-                        index: '2',
-                        title: '表格',
-                        subs: [
-                            {
-                                index: 'basetable',
-                                title: '基础表格'
-                            },
-                            {
-                                index: 'vuetable',
-                                title: 'Vue表格组件'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-date',
-                        index: '3',
-                        title: '表单',
-                        subs: [
-                            {
-                                index: 'baseform',
-                                title: '基本表单'
-                            },
-                            {
-                                index: 'vueeditor',
-                                title: '编辑器'
-                            },
-                            {
-                                index: 'markdown',
-                                title: 'markdown'
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-on',
-                        index: 'basecharts',
-                        title: '图表'
-                    },
-                    {
-                        icon: 'el-icon-upload2',
-                        index: 'drag',
-                        title: '拖拽'
-                    }
-                ]
-            }
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: "el-icon-setting",
+          index: "readme",
+          title: "自述"
         },
-        computed:{
-            onRoutes(){
-                return this.$route.path.replace('/','');
+        {
+          icon: "el-icon-menu",
+          index: "2",
+          title: "表格",
+          subs: [
+            {
+              index: "basetable",
+              title: "基础表格"
+            },
+            {
+              index: "vuetable",
+              title: "Vue表格组件"
             }
+          ]
+        },
+        {
+          icon: "el-icon-date",
+          index: "3",
+          title: "表单",
+          subs: [
+            {
+              index: "baseform",
+              title: "基本表单"
+            },
+            {
+              index: "vueeditor",
+              title: "编辑器"
+            },
+            {
+              index: "markdown",
+              title: "markdown"
+            },
+            {
+              index: "upload",
+              title: "文件上传"
+            }
+          ]
+        },
+        {
+          icon: "el-icon-star-on",
+          index: "basecharts",
+          title: "图表"
+        },
+        {
+          icon: "el-icon-upload2",
+          index: "drag",
+          title: "拖拽"
         }
+      ]
+    };
+  },
+  computed: {
+    onRoutes() {
+      return this.$route.path.replace("/", "");
     }
+  }
+};
 </script>
 
 <style scoped>
-    .sidebar{
-        display: block;
-        position: absolute;
-        width: 250px;
-        left: 0;
-        top: 70px;
-        bottom:0;
-        background: #2E363F;
-    }
-    .sidebar > ul {
-        height:100%;
-    }
+.sidebar {
+  display: block;
+  position: absolute;
+  width: 250px;
+  left: 0;
+  top: 70px;
+  bottom: 0;
+  background: #2e363f;
+}
+.sidebar > ul {
+  height: 100%;
+}
 </style>
